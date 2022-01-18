c5e35abadb9c7c8ba458cda1620477f2f17642e4
icedlee337 <lee.e.norris@gmail.com>
2022-01-18 10:21:40 -0500

    fix: remove main

--- src/lib/server-kafka.ts
+++ src/lib/server-kafka.ts
@@ -2,7 +2,7 @@ import { Kafka } from 'kafkajs';
 import { CompressionCodecs, CompressionTypes } from 'kafkajs';
 import LZ4Codec from 'kafkajs-lz4';
 
-export async function serverKafka(options: { brokers: string[], clientId: string, eachMessage: any, fromBeginning: boolean, groupId: string, topic: string, lz4: boolean, createProducer: boolean}) {
+export async function serverKafka(options: { brokers: string[], clientId: string, eachMessage: any, fromBeginning: boolean, groupId: string, topic: string, lz4?: boolean, createProducer?: boolean}) {
   if (options.lz4) CompressionCodecs[CompressionTypes.LZ4] = new LZ4Codec().codec;
   const { brokers, clientId, eachMessage, fromBeginning, groupId, topic } = options;
   const kafka = new Kafka({ clientId, brokers });
